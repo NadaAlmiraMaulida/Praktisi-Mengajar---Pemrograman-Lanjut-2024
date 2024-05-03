6363b323e4d6f5bdd33f18433a7e3cdb7f558652
Nada Almira Maulida <160992073+NadaAlmiraMaulida@users.noreply.github.com>
2024-05-03 20:20:53 +0700

    Update mydataObj.java

--- Java Generic/mydataObj.java
+++ Java Generic/mydataObj.java
@@ -1,1 +1,21 @@
+package PRAKTISI;
+
+public class mydataObj {
+    public static void main(String[] args) {
+        mydata<String> myDataa  = new mydata<>("nada");
+        System.out.println(myDataa.getData());
+
+        mydata<Integer> intData = new mydata<>(10);
+        mydata<String> strData = new mydata<>("Hello, World!");
+
+        System.out.println("Integer data: " + intData.getData());
+        System.out.println("String data: " + strData.getData());
+
+        intData.setData(20);
+        strData.setData("Updated");
+
+        System.out.println("Updated Integer data: " + intData.getData());
+        System.out.println("Updated String data: " + strData.getData());
+    }
+}
 
