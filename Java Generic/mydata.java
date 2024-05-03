47b9b9f998ccb31f370d91e1d842e89fa70e286c
Nada Almira Maulida <160992073+NadaAlmiraMaulida@users.noreply.github.com>
2024-05-03 20:20:01 +0700

    Update mydata.java

--- Java Generic/mydata.java
+++ Java Generic/mydata.java
@@ -1,1 +1,19 @@
+package PRAKTISI;
+
+public class mydata <T>{
+    private T data;
+
+
+public mydata (T data){
+    this.data = data;
+    }
+
+public T getData(){
+    return data;
+}
+
+public void setData(T data){
+    this.data = data;
+}
+}
 
