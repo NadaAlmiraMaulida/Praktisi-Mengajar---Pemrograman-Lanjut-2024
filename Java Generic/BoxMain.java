f8fdc7cdfd7ef35f61839054e87e99a4e9c91009
Nada Almira Maulida <160992073+NadaAlmiraMaulida@users.noreply.github.com>
2024-05-03 20:23:29 +0700

    Update BoxMain.java

--- Java Generic/BoxMain.java
+++ Java Generic/BoxMain.java
@@ -1,1 +1,13 @@
+public class BoxMain {
+    public static void main(String[] args) {
+        
+Box<String> stringBox = new Box<>("Hello World");
+String message = stringBox.getItem();
+System.out.println(message);
 
+Box<Integer> integerBox = new Box<>(10);
+int number = integerBox.getItem();
+System.out.println(number); 
+    }
+    
+}
