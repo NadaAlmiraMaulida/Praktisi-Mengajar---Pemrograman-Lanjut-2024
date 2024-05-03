679296ec2b2bce9a30b95d1b3689434701167ece
Nada Almira Maulida <160992073+NadaAlmiraMaulida@users.noreply.github.com>
2024-05-03 20:15:05 +0700

    Update PertemuanKedua.java

--- Java Collection/PertemuanKedua.java
+++ Java Collection/PertemuanKedua.java
@@ -1,1 +1,67 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
+
+public class PertemuanKedua {
+    public static void main(String[] args) {
+        // Iterable<String> names = List.of("nada","almira");
+        // Iterator<String> iterator = names.iterator();
+
+        // for(var name: names) {
+        //     System.out.println(name);
+        // }
+
+        // System.out.println("==========");
+        // System.out.println("menampilkan data dengan teknik iterator");
+        // while (iterator.hasNext()) {
+        //     String name = iterator.next();
+        //     System.out.println(name);
+            
+        // }
+
+        // Collection<String> names = new ArrayList<>();
+        // names.add("nada");
+        // names.add("almira");
+        // names.addAll(Arrays.asList("nada", "nidi", "nudu"));
+
+        // names.remove("nudu");
+
+        // System.out.println(names.contains("nudu")); //buat ngecek data
+
+        // for(var name: names){
+        //     System.out.println(name);
+        // }
+
+        // List<String> names = new LinkedList<>();
+        // names.add("nada");
+        // names.add("almira");
+
+        // for(var name: names){
+        //     System.out.println(names);
+        // }
+
+        // Queue<String> names = new ArrayDeque<>();
+        // names.offer("nada");
+
+        // for(String next = names.poll(); next != null; next = names.poll()) {
+
+        // }
+        // System.out.println(names.size());
+
+        // Deque<String> names = new ArrayDeque<>();
+        // names.offerLast("nada");
+        // names.offerLast("almira");
+        // names.offerLast("maulida");
+
+        // for(String next = names.pollLast(); next != null; next = names.pollLast()){
+        //     System.out.println(next);
+        // }
+    }
+}
